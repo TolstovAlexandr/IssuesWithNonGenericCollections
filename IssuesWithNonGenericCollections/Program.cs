b56e2a01b55f1788ac891dcc9c564edf5206ccc3
Alexandr Tolstov <SESA574650@se.com>
2020-11-03 09:16:35 +0600

    Add project files.

---
+++ IssuesWithNonGenericCollections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace IssuesWithNonGenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void SimpleBoxUnboxOperation()
+        {
+            int myInt = 25;
+            object boxedInt = myInt;
+            int unboxedInt = (int)boxedInt;
+        }
+        static void WorkWithArrayList()
+        {
+            // Value types are automatically boxed when
+            // passed to a method requesting an object.
+            ArrayList myInts = new ArrayList();
+            myInts.Add(10);
+            myInts.Add(20);
+            myInts.Add(35);
+
+            // Unboxing occurs when an object is converted back to
+            // stack-based data.
+            int i = (int)myInts[0];
+            // Now it is reboxed, as WriteLine() requires object types!
+            Console.WriteLine("Value of your int: {0}", i);
+        }
+    }
+}
